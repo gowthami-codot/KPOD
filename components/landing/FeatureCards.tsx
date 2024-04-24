d99357c86c25f2c88daca71fb5bbfba7916282a2
Ankit <abinashdwivedi69@gmail.com>
2024-04-24 22:07:00 +0530

    Added Gradients

--- components/landing/FeatureCards.tsx
+++ components/landing/FeatureCards.tsx
@@ -4,9 +4,9 @@ const FeatureCards = () => {
   return (
     <div className="bg-white text-black">
       <div className="py-10 md:py-20 px-5 md:px-40">
-        <div className="flex flex-col md:flex-row gap-3 md:gap-6 items-center">
+        <div className="flex flex-col md:flex-row gap-3 md:gap-6 items-center flex-grow">
           <div className="w-full md:w-[70%] flex flex-col gap-3 md:gap-6">
-            <div className="border border-gray-100 p-4 md:p-10 rounded-2xl">
+            <div className="border border-gray-200 p-4 md:p-10 rounded-2xl">
               <div className="text-lg font-bold">High-Performance GPUs</div>
               <div className="text-xs my-3 md:my-6">
                 Offer state-of-the-art GPU hardware that provides top-tier
@@ -15,7 +15,7 @@ const FeatureCards = () => {
               </div>
             </div>
             <div className="flex flex-col md:flex-row gap-3 md:gap-6">
-              <div className="w-full border border-gray-100 p-4 md:p-10 rounded-2xl">
+              <div className="w-full border border-gray-200 p-4 md:p-10 rounded-2xl">
                 <div className="text-lg font-bold">
                   Real-Time Analytics and Monitoring
                 </div>
@@ -25,7 +25,7 @@ const FeatureCards = () => {
                   and control costs.
                 </div>
               </div>
-              <div className="w-full border border-gray-100 p-4 md:p-10 rounded-2xl relative">
+              <div className="w-full border border-gray-200 p-4 md:p-10 rounded-2xl relative">
                 <div className="text-lg font-bold">Scalable Resources</div>
                 <div className="text-xs my-3 md:my-6">
                   Enable users to scale their GPU resources up or down instantly
@@ -35,21 +35,22 @@ const FeatureCards = () => {
               </div>
             </div>
           </div>
-          <div className="md:w-[30%] flex flex-col w-full gap-3 md:gap-6">
-            <div className="w-full border border-gray-100 p-4 md:p-10 rounded-2xl">
-              <div className="text-lg font-bold">Scalable Resources</div>
+          <div className="md:w-[30%] flex flex-col w-full gap-3">
+            <div className="w-full border border-gray-200 p-4 md:p-10 rounded-2xl">
+              <div className="text-lg font-bold">User-Friendly Interface</div>
               <div className="text-xs my-3 md:my-6">
-                Enable users to scale their GPU resources up or down instantly
-                depending on their project needs, which provides flexibility and
-                cost-effectiveness.
+                Offer a simple, intuitive interface that allows users to manage
+                their GPU resources easily, without needing in-depth technical
+                knowledge.
               </div>
             </div>
-            <div className="w-full border border-gray-100 p-4 md:p-10 rounded-2xl">
-              <div className="text-lg font-bold">Scalable Resources</div>
+            <div className="w-full border border-gray-200 p-4 md:p-10 rounded-2xl">
+              <div className="text-lg font-bold">
+                Advanced Security Measures
+              </div>
               <div className="text-xs my-3 md:my-6">
-                Enable users to scale their GPU resources up or down instantly
-                depending on their project needs, which provides flexibility and
-                cost-effectiveness.
+                Implement top-notch security protocols to protect user data and
+                operations from unauthorized access and threats.
               </div>
             </div>
           </div>
--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 const Features = () => {
   return (
-    <div className="z-10 mt-[100vh] bg-opacity-20 bg-black max-w-screen">
+    <div className="z-10 mt-[100vh] bg-opacity-20 bg-black w-[100%]">
       <div
-        className="bg-black flex flex-col md:flex-row w-full
+        className="flex flex-col md:flex-row 
       gap-6 md:gap-10 items-center justify-between px-5 md:px-10 py-10 md:py-32"
       >
         <div
           className="border border-[#63E3E3] bg-[#ffffff10] p-5
-        rounded-2xl flex justify-between"
+        rounded-2xl flex justify-between w-full z-10"
         >
-          <div className="flex flex-col gap-6 w-full">
+          <div className="flex flex-col gap-6">
             <div className="text-3xl md:text-4xl font-bold">Code</div>
             <div className="text-lg">Code your idea in Notebook</div>
           </div>
@@ -20,14 +20,21 @@ const Features = () => {
             width={150}
             height={150}
             alt="code"
-            className="max-w-[150px]"
+          />
+        </div>
+        <div className="w-[400px] h-[400px] absolute -z-0 md:ml-[20%] md:mt-[10%]]">
+          <Image 
+            src={"/blue_circle.svg"}
+            alt="blue"
+            width={300}
+            height={300}
           />
         </div>
         <div
           className="border border-[#63E3E3] bg-[#ffffff10] p-5 w-full
-        rounded-2xl flex justify-between"
+        rounded-2xl flex justify-between z-10"
         >
-          <div className="flex flex-col gap-6 w-full">
+          <div className="flex flex-col gap-6">
             <div className="text-3xl md:text-4xl font-bold">Train</div>
             <div className="text-lg">Train your models with machines</div>
           </div>
@@ -36,14 +43,23 @@ const Features = () => {
             width={150}
             height={150}
             alt="code"
-            className="max-w-[150px]"
+            className="object-cover"
+          />
+        </div>
+        <div className="w-[500px] h-[500px] absolute -z-0 md:ml-[50%] md:mt-[20%]">
+          <Image 
+            src={"/green_circle.svg"}
+            alt="blue"
+            width={300}
+            height={300}
+            className="object-cover"
           />
         </div>
         <div
           className="border border-[#63E3E3] bg-[#ffffff10] p-5 w-full
-        rounded-2xl flex justify-between"
+        rounded-2xl flex justify-between overflow-hidden z-10"
         >
-          <div className="flex flex-col gap-6 w-full">
+          <div className="flex flex-col gap-6">
             <div className="text-3xl md:text-4xl font-bold">Implement</div>
             <div className="text-lg">Make it live</div>
           </div>
@@ -52,7 +68,7 @@ const Features = () => {
             width={150}
             height={150}
             alt="code"
-            className="max-w-[150px]"
+            className="w-[140px]"
           />
         </div>
       </div>
