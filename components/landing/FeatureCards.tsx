bc096dcbc942a699165d06ae5db45bb76aaa20fe
Ankit <abinashdwivedi69@gmail.com>
2024-04-25 19:34:29 +0530

    bg video added

--- components/landing/FeatureCards.tsx
+++ components/landing/FeatureCards.tsx
@@ -5,9 +5,9 @@ const poppins = Poppins({ subsets: ["latin"], weight: "200" });
 const FeatureCards = () => {
   return (
     <div className="bg-white text-black">
-      <div className="py-10 md:py-20 px-5 md:px-40">
+      <div className="py-10 md:py-20 px-5 md:px-40 relative">
         <div className="flex flex-col md:flex-row gap-3 md:gap-6 items-stretch">
-          <div className="w-full md:w-[65%] flex flex-col gap-3 md:gap-6 items-stretch">
+          <div className="w-full md:w-[65%] flex flex-col gap-3 md:gap-6 items-stretch z-10">
             <div className="border border-gray-200 p-4 md:p-10 rounded-2xl">
               <div className="text-lg font-bold">High-Performance GPUs</div>
               <div className={`${poppins.className} text-md my-3 md:my-6`}>
@@ -37,7 +37,7 @@ const FeatureCards = () => {
               </div>
             </div>
           </div>
-          <div className="md:w-[35%] flex flex-col w-full gap-3 md:gap-6">
+          <div className="md:w-[35%] flex flex-col w-full gap-3 md:gap-6 z-10">
             <div className="w-full border border-gray-200 p-4 md:p-10 rounded-2xl">
               <div className="text-lg font-bold">User-Friendly Interface</div>
               <div className={`${poppins.className} text-md my-3 md:my-6`}>
@@ -55,6 +55,9 @@ const FeatureCards = () => {
                 operations from unauthorized access and threats.
               </div>
             </div>
+          </div>
+          <div className="absolute inset-0">
+            <video src="/krutrim_bg_video.mp4" autoPlay loop muted className="absolute inset-0 w-full h-full object-cover" />
           </div>
         </div>
       </div>
