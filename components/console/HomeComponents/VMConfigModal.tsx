14ce44f055d7195e5377783ff2ada9eab9edd498
Ankit <abinashdwivedi69@gmail.com>
2024-04-27 16:28:28 +0530

    drop downs removed

--- components/console/HomeComponents/VMConfigModal.tsx
+++ components/console/HomeComponents/VMConfigModal.tsx
@@ -30,40 +30,17 @@ const VMConfigModal = ({ isOpen }: any) => {
                   <div className="text-lg md:text-2xl font-bold">
                     Tell us about your VM Configuration
                   </div>
-                  <form className="mt-3 md:mt-6 flex flex-col gap-4">
-                    <select className="text-white p-4 rounded-xl bg-[#FFFFFF20] w-full border border-gray-300">
-                      <option disabled className="text-black">
-                        VM Type
-                      </option>
-                      <option value="vm-type-1" className="text-black">
-                        VM Type 1(16 Cores -4GHz, 16GB memory, SSD)
-                      </option>
-                      <option value="vm-type-2" className="text-black">
-                        VM Type 2(16 Cores -4GHz, 32GB memory, SSD)
-                      </option>
-                      <option value="vm-type-3" className="text-black">
-                        VM Type 3(32 Cores -4GHz, 64GB memory, SSD)
-                      </option>
-                    </select>
-                    <select className="text-white my-3 p-4 rounded-xl bg-[#FFFFFF20] w-full border border-gray-300">
-                      <option disabled className="text-black">
-                        OS
-                      </option>
-                      <option value="windows" className="text-black">
-                        Windows
-                      </option>
-                      <option value="linux" className="text-black">
-                        Linux
-                      </option>
-                    </select>
-                    <input
-                      type="text"
-                      placeholder="Your ssh key"
-                      className=" p-4 rounded-xl bg-[#FFFFFF20] w-full border border-gray-300"
-                    />
-                  </form>
+                  <input
+                    type="text"
+                    placeholder="Your ssh key"
+                    className="p-4 rounded-xl bg-[#FFFFFF20] my-5 w-full border border-gray-300"
+                  />
                 </div>
-                <Link href={"/ssh-keygen"} target="_blank" className="text-center text-[#00BAFF] cursor-pointer my-2 hover:underline">
+                <Link
+                  href={"/ssh-keygen"}
+                  target="_blank"
+                  className="text-center text-[#00BAFF] cursor-pointer my-2 hover:underline"
+                >
                   How to generate private SSH Key
                 </Link>
               </ModalBody>
