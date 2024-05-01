93a5979d391823cce0b284894cce2ddf59ebaaa5
Gowthami <gowthami.d@equinoxtechsolutions.com>
2024-05-01 09:54:53 +0530

    buttons usage

--- components/console/InferenceServices/Usage.jsx
+++ components/console/InferenceServices/Usage.jsx
@@ -46,13 +46,13 @@ const Usage = () => {
           <div className="text-[27px]  font-bold mt-6 px-10  text-[#8BFFDD] ">
             Usage
           </div>
-          <div className="flex items-end justify-end pl-[29rem] ">
+          <div className="flex items-end justify-end pl-[23rem] ">
             {" "}
             <div
-              className="flex justify-end  bg-[#FFFFFF4D] bg-opacity-30 mt-8 px-3 mr-10 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-5 cursor-pointer"
+              className="flex justify-end  bg-[#FFFFFF4D] bg-opacity-30 mt-8 px-2 mr-4 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-2 cursor-pointer"
               onClick={handleCreateApiClick}
             >
-              <div className="text-[18px]">Models</div>
+              <div className="text-[17px] pb-[2px]">Models</div>
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -60,21 +60,22 @@ const Usage = () => {
                 width="30"
                 height="30"
                 fill="white"
+                className="pb-1"
               >
                 <path d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 17.29L18.29 11H5.71z" />
               </svg>
             </div>
           </div>
-          <div className="month-container flex justify-end  bg-[#FFFFFF4D] bg-opacity-30 mt-10  px-3 mr-10 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-5 cursor-pointer">
+          <div className="month-container flex justify-end  bg-[#FFFFFF4D] bg-opacity-30 mt-10  px-2 mr-4 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-5 cursor-pointer">
   <div className="angle left-angle text-[21px] cursor-pointer" onClick={() => handleClick('left')}>&lt;</div>
   <div className="month text-[17px] text-center pb-1">{months[currentMonthIndex]}</div>
   <div className="angle text-[21px] right-angle cursor-pointer" onClick={() => handleClick('right')}>&gt;</div>
 </div>
 
 
-          <div className="flex justify-end  bg-[#80FFF7] text-[17px] text-black font-bold mt-10  px-3 mr-10 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-5 cursor-pointer">
-            <div className="px-6">Export</div>
+          <div className="flex justify-end  bg-[#80FFF7] text-[17px] text-black font-bold mt-10  px-2 mr-4 rounded-[13px] border-[#FFFFFF] border-[2px]  py-1 items-end space-x-5 cursor-pointer">
+            <div className="px-4 pb-[2px]">Export</div>
           </div>
         </div>
         <div className="flex flex-col md:flex-row gap-4 p-10">
