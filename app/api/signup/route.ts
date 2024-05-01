2dfa147bfe2704b3473fc35dc53d9871a9b4be7f
Sun Jian <sun.jian@olakrutrim.com>
2024-05-01 14:51:31 +0800

    remove sendVerification from public API

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -34,7 +34,39 @@ export async function POST(request: Request) {
       const result = await response.json();
       return NextResponse.json(result);
     } else if (response.ok) {
-      return NextResponse.json({ message: "Signup successful" });
+
+      try {
+        const verificationData = {
+          email,
+          "mailTemplate": "registration"
+        };
+
+        const response = await fetch(`${process.env.API_BASE_URL}/sendmail`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(verificationData),
+        });
+
+        if (response.status === 200) {
+          return NextResponse.json({ message: "Signup successful" });
+        } else {
+          console.error("An error occurred while processing your request. Status:");
+          return NextResponse.json({
+            message: "An error occurred while processing your request.",
+          });
+        }
+      } catch (error) {
+        console.error("Caught an error:", error);
+        return NextResponse.json({
+          message: "An error occurred while processing your request.",
+        });
+      }
+
+
+
+
     } else {
       return NextResponse.json({
         message: "An error occurred while processing your request.",
