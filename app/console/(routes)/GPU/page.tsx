feb255014b0a3c7574b5d23532c4daecd06945f5
Gowthami <gowthami.d@equinoxtechsolutions.com>
2024-05-03 10:07:52 +0530

    GPU page

--- app/console/(routes)/GPU/page.tsx
+++ app/console/(routes)/GPU/page.tsx
@@ -5,23 +5,25 @@ const page = () => {
   const { userDetails } = useUser();
 
   return (
-    <div className="px-10 md:px-0">
+   <div className="bg-white min-h-screen flex justify-center items-center">
+     
       <div
-        className="flex flex-col items-center justify-center py-6 md:py-10 mx-auto mt-20 md:mt-40
-md:w-[78%] w-full border border-[#63e3e3] rounded-3xl px-10 gap-6"
+        className="flex flex-col  bg-[#F3F4F5] items-center justify-center py-6 md:py-10 mx-auto  
+md:w-[78%] w-full border-[2px] border-[#F3F4F5]  rounded-[8px] px-20 md:px-10 gap-6"
       >
-        <div className="text-xl md:text-3xl font-bold text-center">
+        <div className="text-xl md:text-3xl text-black font-bold text-center">
           Welcome to OLA Krutrim GPU Instance service
         </div>
         <div className="text-gray-400 text-center">
-        <span className="text-xl text-white">Your GPU Instance is live now</span>  <br />
-        <br /> You can login into your GPU instance using the ssh
+        <span className="md:text-xl text-black  text-[12px] ">Your GPU Instance is live now</span>  <br />
+        <br /><div className="md:text-[16px] text-[12px] md:ml-0 ml-12"> You can login into your GPU instance using the ssh
           command details that has been emailed to you <br /> <br /> If you face any issues
           or need support, <br />please feel free to contact
-          <span className="text-blue-400 mx-1">cloudsupport@olakrutrim.com</span>
+          <span className="text-blue-400 mx-1">cloudsupport@olakrutrim.com</span></div>
         </div>
       </div>
     </div>
+  
   );
 };
 
