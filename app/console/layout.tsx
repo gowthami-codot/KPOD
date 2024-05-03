51c0c29ca3341eb0d927b3044013d9dd62620bad
Ankit <abinashdwivedi69@gmail.com>
2024-05-03 11:11:22 +0530

    Menu toggle ham

--- app/console/layout.tsx
+++ app/console/layout.tsx
@@ -1,16 +1,57 @@
 "use client";
 import useProtectedRoute from "@/components/auth/ProtectedRoutes";
+import { useState } from "react";
 import SideBar from "@/components/nav/SideBar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-
+  const [showSidebar, setShowSidebar] = useState(false);
   useProtectedRoute();
 
   return (
     <div className="flex bg-white">
-    <div className="w-[80px] h-[100vh] bg-white text-darkPrimary">
-      <SideBar />
-    </div>
+      <div
+        className={`w-[80px] h-[100vh] bg-white text-darkPrimary ${
+          showSidebar ? "flex" : "hidden"
+        }`}
+      >
+        <SideBar />
+      </div>
+      <div
+        className="text-black flex md:hidden absolute mt-5 mr-5 right-0"
+        onClick={() => setShowSidebar(!showSidebar)}
+      >
+        {showSidebar ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        )}
+      </div>
       <div className="h-full flex flex-col w-full">
         <main className="">{children}</main>
       </div>
